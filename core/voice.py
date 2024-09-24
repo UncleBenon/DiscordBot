4127e8857efa516348f8663a596bba0f9c7d43f3
UncleBenon <burningvenom@gmail.com>
2024-09-24 21:15:58 +0200

    upped the timeout

--- core/voice.py
+++ core/voice.py
@@ -32,7 +32,7 @@ async def voiceSynthFunction(prompt : str, debug = False) -> str:
         while not await found.is_visible():
             await sleep(1)
             _cc += 1
-            if _cc >= 120:
+            if _cc >= 300:
                 raise Exception("timed out")
             if await page.get_by_text("Error").first.is_visible():
                 if _errorforce > 4:
