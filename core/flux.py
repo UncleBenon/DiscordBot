37db991f57beee6c3934d7f9a109dbcfc79ce7de
UncleBenon <burningvenom@gmail.com>
2024-10-15 12:36:16 +0200

    fixed flux

--- core/flux.py
+++ core/flux.py
@@ -17,23 +17,23 @@ async def fluxMasterFunction(prompt : str, DEBUG = False):
         if await page.get_by_text("Your space is in error").is_visible():
             raise Exception("Space is having errors, not the bot's fault")
 
+        await sleep(1)
         await page.get_by_test_id("textbox").fill(prompt)
         await page.get_by_role("button", name="Run").click()
 
         imgs = await page.query_selector_all('img')
         _cc = 0
         _error = 0
-        while len(imgs) <= 4:
+        while len(imgs) < 2:
             await sleep(1)
             _cc += 1
             if _cc >= 120:
                 raise Exception("timed out")
             if await page.get_by_text("Error").first.is_visible():
-                if _error > 10:
+                if _error >= 10:
                     raise Exception("Error!")
+                _cc = 0
                 _error += 1
-                await page.get_by_role("button", name="Run").click()
-                await sleep(1)
             imgs = await page.query_selector_all('img')
         for found in imgs:
             link = await found.get_attribute('src')
