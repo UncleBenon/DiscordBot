3d9f88de0af9b9f267633250b14a04316f10e72f
UncleBenon <burningvenom@gmail.com>
2025-08-20 20:38:29 +0200

    it didn't

--- commands.py
+++ commands.py
@@ -716,7 +716,7 @@ class ChatCommands(commands.Cog):
             return
 
     @commands.hybrid_command(
-        name="TwitVid",
+        name="Twitvid",
         description="Downloads a video of twitter, if i did this correctly.",
     )
     async def twitvidcommand(self, ctx : commands.Context, url : str):
