8f51395ab67ef4ca8bf76ba8ca396a719efc5eb4
UncleBenon <burningvenom@gmail.com>
2025-09-03 11:11:56 +0200

    bruh

--- core/twitterDownload.py
+++ core/twitterDownload.py
@@ -6,7 +6,7 @@ import requests
 import os
 
 DIR_PATH = "temp"
-HEADERS = headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 async def downloadTwitterVideoFunction(URL):
     if not URL.startswith("https://x.com/"):
