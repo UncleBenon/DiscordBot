4ce9a7a93b714c35e5377b79364cc64d098354b6
UncleBenon <burningvenom@gmail.com>
2026-05-11 09:28:33 +0200

    oops

--- commands.py
+++ commands.py
@@ -700,7 +700,7 @@ class ChatCommands(commands.Cog):
             return
 
         await self.DEBUG_CHANNEL.send(
-            f"{curTime()}  -  {ctx.author} used the voice synth 2 command\nVoice: {voice}\nTemp: {temp}\n\n{prompt[:1000]}"
+            f"{curTime()}  -  {ctx.author} used the voice synth 2 command\nVoice: {VOICES[int(voice)]}\nTemp: {temp}\n\n{prompt[:1000]}"
         )
         print(f"{curTime()}  -  {ctx.author} used the voice synth 2 command")
 
