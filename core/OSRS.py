ecc0ad67d7cd4794c2398d059360c09f8869a4a2
UncleBenon <burningvenom@gmail.com>
2026-07-26 16:47:21 +0200

    fix.

--- core/OSRS.py
+++ core/OSRS.py
@@ -1,10 +1,15 @@
-from playwright.async_api import async_playwright 
+from playwright.async_api import async_playwright
 
-async def getBondPriceOSRS(debug : bool = False) -> tuple[str,str]:
+
+async def getBondPriceOSRS(debug: bool = False) -> tuple[str, str]:
     async with async_playwright() as p:
         driver = await p.firefox.launch(headless=not debug)
         page = await driver.new_page()
         await page.goto("https://prices.runescape.wiki/osrs/item/13190")
-        buyPrice = await page.locator("#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(1) > div:nth-child(2) > span").inner_text()
-        sellPrice = await page.locator("#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(2) > div:nth-child(2) > span").inner_text()
+        buyPrice = await page.locator(
+            "#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(2) > div:nth-child(2) > span"
+        ).inner_text()
+        sellPrice = await page.locator(
+            "#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(1) > div:nth-child(2) > span"
+        ).inner_text()
         return (sellPrice, buyPrice)
