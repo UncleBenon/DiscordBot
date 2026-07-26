aa71164d20a8ab0d44c4bb6baa0a23b8b3efbf7c
UncleBenon <burningvenom@gmail.com>
2026-07-26 20:58:16 +0200

    fix.

--- core/OSRS.py
+++ core/OSRS.py
@@ -7,9 +7,9 @@ async def getBondPriceOSRS(debug: bool = False) -> tuple[str, str]:
         page = await driver.new_page()
         await page.goto("https://prices.runescape.wiki/osrs/item/13190")
         buyPrice = await page.locator(
-            "#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(2) > div:nth-child(2) > span"
+            "#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(1) > div:nth-child(2) > span"
         ).inner_text()
         sellPrice = await page.locator(
-            "#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(1) > div:nth-child(2) > span"
+            "#root > div > main > div > div.item-info-container > div.item-info-row > div.item-info-prices > div:nth-child(2) > div:nth-child(2) > span"
         ).inner_text()
         return (sellPrice, buyPrice)
